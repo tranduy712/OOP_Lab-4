e8d278bbee1bbd94c2898c4cc1ba40650c7ef065
tranduy712 <71444587+tranduy712@users.noreply.github.com>
2020-11-03 20:57:33 +0700

    Update DrawPanel.java

--- Lab #4/src/question2/DrawPanel.java
+++ Lab #4/src/question2/DrawPanel.java
@@ -4,7 +4,6 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class DrawPanel extends JPanel {
-    // draw an X from the corners of the panel
     public void paintComponent(Graphics g) {
         // call paintComponent to ensure the panel displays correctly
         super.paintComponent(g);
